417a767ceaf105e721324394b3dd2ec6838b1fbe
erikamjas11 <67701747+erikamjas11@users.noreply.github.com>
2024-01-08 21:54:28 -0500

    Add files via upload
    Hello, Erik

---
+++ myHello.c
@@ -0,0 +1,10 @@
+/* Author: Erik Marrero
+Date:8/30/2020
+Description:Write a C program that writes "Hello yourname" to the monitor.
+ */
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello, Erik!\n");
+}
